a95b878012bbd9ecba16cac561f88965c1d3c481
hburn7 <hburnett777@gmail.com>
2025-08-15 22:21:03 -0400

    Use ${ENVIRONMENT} in otr-processor-job.sh

--- src/processor/otr-processor-job.sh
+++ src/processor/otr-processor-job.sh
@@ -11,12 +11,12 @@ echo 'Dataworkerservice stopped'
 
 echo 'Running otr-processor:Staging via docker'
 
-docker run --network host -e CONNECTION_STRING="postgresql://${DATABASE_USER}:${DATABASE_PASSWORD}@localhost:5432/${DATABASE_NAME}" -e "RABBITMQ_URL=${RABBITMQ_URL}" -e RUST_LOG=info --rm stagecodes/otr-processor:production
+docker run --network host -e CONNECTION_STRING="postgresql://${DATABASE_USER}:${DATABASE_PASSWORD}@localhost:5432/${DATABASE_NAME}" -e "RABBITMQ_URL=${RABBITMQ_URL}" -e RUST_LOG=info --rm "stagecodes/otr-processor:${ENVIRONMENT}"
 
 echo 'Restarting dataworkerservice'
 docker start dataworkerservice
 echo 'dataworkerservice restarted'
 
-docker rmi stagecodes/otr-processor:production
+docker rmi "stagecodes/otr-processor:${ENVIRONMENT}"
 
 echo 'Finished'
