ed739fe37a3a04051f79cf773d55d82e67468924
hburn7 <hburnett777@gmail.com>
2025-08-15 22:25:23 -0400

    fix: remove stop/start of legacy dataworkerservice

--- src/processor/otr-processor-job.sh
+++ src/processor/otr-processor-job.sh
@@ -4,19 +4,9 @@
 # shellcheck source=/dev/null
 source "${HOME}/otr-scripts/src/load-env.sh"
 
-echo 'Stopping dataworkerservice'
-
-docker stop dataworkerservice
-echo 'Dataworkerservice stopped'
-
 echo 'Running otr-processor:Staging via docker'
 
 docker run --network host -e CONNECTION_STRING="postgresql://${DATABASE_USER}:${DATABASE_PASSWORD}@localhost:5432/${DATABASE_NAME}" -e "RABBITMQ_URL=${RABBITMQ_URL}" -e RUST_LOG=info --rm "stagecodes/otr-processor:${ENVIRONMENT}"
-
-echo 'Restarting dataworkerservice'
-docker start dataworkerservice
-echo 'dataworkerservice restarted'
-
 docker rmi "stagecodes/otr-processor:${ENVIRONMENT}"
 
 echo 'Finished'
