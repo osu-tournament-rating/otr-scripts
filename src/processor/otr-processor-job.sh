39f8de2f78c4575437da7495f9e91eb6a06f6f9f
hburn7 <hburnett777@gmail.com>
2025-08-23 17:11:35 -0400

    Use latest tag for processor job

--- src/processor/otr-processor-job.sh
+++ src/processor/otr-processor-job.sh
@@ -6,7 +6,13 @@ source "${HOME}/otr-scripts/src/load-env.sh"
 
 echo 'Running otr-processor:Staging via docker'
 
-docker run --network host -e CONNECTION_STRING="postgresql://${DATABASE_USER}:${DATABASE_PASSWORD}@localhost:5432/${DATABASE_NAME}" -e "RABBITMQ_URL=${RABBITMQ_URL}" -e RUST_LOG=info --rm "stagecodes/otr-processor:${ENVIRONMENT}"
+TAG=$ENVIRONMENT
+# If the environment is equal to "production", use "latest" as the tag instead of ${ENVIRONMENT}
+if [[ $ENVIRONMENT == "production" ]]; then
+    TAG=latest
+fi
+
+docker run --network host -e CONNECTION_STRING="postgresql://${DATABASE_USER}:${DATABASE_PASSWORD}@localhost:5432/${DATABASE_NAME}" -e "RABBITMQ_URL=${RABBITMQ_URL}" -e RUST_LOG=info --rm "stagecodes/otr-processor:${TAG}"
 docker rmi "stagecodes/otr-processor:${ENVIRONMENT}"
 
 echo 'Finished'
