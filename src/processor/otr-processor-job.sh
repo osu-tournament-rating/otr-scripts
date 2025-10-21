f5e5a8bc6d530756cb8f091901c408a75f933344
hburn7 <hburnett777@gmail.com>
2025-10-21 17:44:30 -0400

    Fix print out

--- src/processor/otr-processor-job.sh
+++ src/processor/otr-processor-job.sh
@@ -4,13 +4,13 @@
 # shellcheck source=/dev/null
 source "${HOME}/otr-scripts/src/load-env.sh"
 
-echo 'Running otr-processor:Staging via docker'
-
 TAG=$ENVIRONMENT
 # If the environment is equal to "production", use "latest" as the tag instead of ${ENVIRONMENT}
 if [[ $ENVIRONMENT == "production" ]]; then
     TAG=latest
 fi
+
+echo "Running otr-processor:${TAG} via docker"
 
 docker run --network host -e CONNECTION_STRING="postgresql://${DATABASE_USER}:${DATABASE_PASSWORD}@localhost:5432/${DATABASE_NAME}" -e "RABBITMQ_URL=${RABBITMQ_URL}" -e RUST_LOG=info --rm "stagecodes/otr-processor:${TAG}"
 docker rmi "stagecodes/otr-processor:${ENVIRONMENT}"
