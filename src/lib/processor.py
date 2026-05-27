6655dcd7466cf52a71fc85d5a8493e63b37c2503
hburn7 <hburnett777@gmail.com>
2026-05-27 19:52:10 -0400

    fix processor networking

--- src/lib/processor.py
+++ src/lib/processor.py
@@ -17,11 +17,11 @@ def run():
     for line in pull.stdout.splitlines():
         logger.info(line.decode())
 
-    conn_str = f"postgresql://{config.db_user}:{config.db_password}@localhost:{config.db_port}/{config.db_name}"
-    rust_log = "RUST_LOG=info"
-    rabbit = f"RABBITMQ_URL={config.rabbitmq_url}"
+    conn_str = f"postgresql://{config.db_user}:{config.db_password}@{config.db_container}:{config.db_port}/{config.db_name}"
+    rust_log = "info"
+    rabbit = config.rabbitmq_url
 
-    run_cmd = f"docker run --network host -e CONNECTION_STRING={conn_str} -e RUST_LOG={rust_log} -e RABBITMQ_URL={rabbit} {image}".split()
+    run_cmd = f"docker run -e CONNECTION_STRING={conn_str} -e RUST_LOG={rust_log} -e RABBITMQ_URL={rabbit} {image}".split()
 
     logger.info("Running processor")
     exc = subprocess.run(run_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
